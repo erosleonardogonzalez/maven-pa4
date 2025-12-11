4846bb65b6aa91cec5823cd7165e80ba7313e6cb
Eros <erosleonardogonzalez@usal.es>
2025-12-11 19:10:23 +0100

    subida a github

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.leonardogonzalez","l":"Main"},{"p":"com.leonardogonzalez","l":"Persona"}];updateSearchResults();
